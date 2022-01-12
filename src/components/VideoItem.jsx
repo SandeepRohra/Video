bb9d42c99de7856c512e1932387f52756d1f8564
Sandeep Rohra <sandeep12rohra@gmail.com>
2022-01-12 20:29:51 +0530

    do component banaye and jo app  s ek Video list ki prop aya usko use karke list map kardiye and added notes in videoList.jsx lacture 128

---
+++ src/components/VideoItem.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+const VideoItem = () => {
+  return <div>Video Item</div>
+}
+export default VideoItem
--- src/components/Videolist.jsx
+++ src/components/Videolist.jsx
@@ -1,8 +1,18 @@
 import react from 'react'
+import VideoItem from './VideoItem'
 
-const VideoList = (props) => {
-  // then yaha p us prop ki length co show kar diye
-  return <div>{props.videos.length}</div>
+const VideoList = ({ videos }) => {
+  //ese b kar sakte h
+
+  const renderList = videos.map((video) => {
+    return <VideoItem />
+  })
+  return <div>{renderList}</div>
 }
 
 export default VideoList
+
+//step 1 Apan ek component banaye VideoList and ek component banaye videoItem.jsx
+//step 2 props ko destrucre kiye ki srif video mille
+//step 3 us videos ko map kar diye aur har ek video m VideoItem ko return kar diye
+//step 4// usko ek const m store kiye and then Video list k function m usko return kar diye ek div k andar us const ko
