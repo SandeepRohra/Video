792526058996402e3bed8cf85e85edd4e5e38aa2
Sandeep Rohra <sandeep12rohra@gmail.com>
2022-01-12 17:05:03 +0530

    linked axios with youtube api and installed axios with notes lacture-123

--- src/Apis/youtube.js
+++ src/Apis/youtube.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBTcLjqbP2i-OIZfL9l-k8rKlEBSpzabug'
 // added youtube api key
 
@@ -5,3 +7,13 @@ const KEY = 'AIzaSyBTcLjqbP2i-OIZfL9l-k8rKlEBSpzabug'
 //2)usedyoutube data api
 //3) genrated api key from there
 //4) pasted it here
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3', //last / nii include kiye
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY, //y docs m ention nii h but apan ko key dalni hote h perams m,
+  },
+})
